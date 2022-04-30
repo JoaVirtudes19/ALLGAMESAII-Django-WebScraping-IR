f76ee2630e2f5f404c99675ddef8eee1a64cb384
JoaVirtudes19 <josejoaquinvirtudes@gmail.com>
2022-04-30 11:59:31 +0200

    Cambios de create a bulk_create

--- allGamesAII/web/models.py
+++ allGamesAII/web/models.py
@@ -2,35 +2,33 @@ from django.db import models
 
 # Create your models here.
 
+##Usar sistema de recomendación colab, añadir pantalla de inicio al usuario para seleccionar las categorais que le gustan
+#Podemos añadir una clave primaria
+#Crear los juegos sin los enlaces y guardar en un diccionario para 
+#seguidamente añadirlo después de un bulk (django 3)
 
-class Tienda(models.Model):
-    nombre = models.CharField(max_length=20)
-    def __str__(self):
-        return self.nombre
+
 
 class Plataforma(models.Model):
-    nombre = models.CharField(max_length=20)
+    nombre = models.TextField(primary_key=True)
     def __str__(self):
         return self.nombre
 
 class Genero(models.Model):
-    nombre = models.CharField(max_length=20)
-    tienda = models.ForeignKey(Tienda,on_delete=models.CASCADE)
+    nombre = models.TextField(primary_key=True)
     def __str__(self):
         return self.nombre
 
 
 class Juego(models.Model):
-    #FALTA AÑADIR LA FECHA Y AL CREARLA datetime.date() NO NECESITAMOS LA HORA
     nombre = models.CharField(max_length=30)
-    url = models.CharField(max_length=200)
+    url = models.CharField(max_length=200,primary_key=True )
     urlImg = models.CharField(max_length=200)
     descripcion = models.TextField()
     nota = models.FloatField()
     descuento = models.IntegerField()
-    fecha = models.DateTimeField(null=True)
+    fecha = models.DateField(null=True)
     genero = models.ManyToManyField(Genero)
     plataforma = models.ForeignKey(Plataforma,on_delete=models.CASCADE)
-    tienda = models.ForeignKey(Tienda,on_delete=models.CASCADE)
     def __str__(self):
         return self.nombre
