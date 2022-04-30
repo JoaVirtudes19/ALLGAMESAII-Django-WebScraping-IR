2c1724992956ecf62d9e83f59af8a18249a60bd4
JoaVirtudes19 <josejoaquinvirtudes@gmail.com>
2022-04-30 16:09:56 +0200

    Mezcla entre get_or_create y bluk_create

--- allGamesAII/web/admin.py
+++ allGamesAII/web/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from web.models import Juego,Tienda,Plataforma,Genero
+from web.models import Juego,Plataforma,Genero
 # Register your models here.
 
 admin.site.register(Juego)
-admin.site.register(Tienda)
 admin.site.register(Plataforma)
 admin.site.register(Genero)
